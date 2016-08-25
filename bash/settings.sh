89a09e3b2ba7db087b52979a3419f6e02f95f535
Max Sogin <max.sogin@gmail.com>
2016-08-25 17:12:50 -0400

    Removed my creds from settings. Key used in prev commit is revoked anyway

--- bash/settings.sh
+++ bash/settings.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
  # change this value to your "Project Id" found on the Project Settings -> API page in dashboard
-PROJECT_ID='2fc919755'
+PROJECT_ID=''
 
 # change this value to your "Smartling API key" found on the Project Settings -> API page in dashboard
-API_KEY='cf61a56d-1ef1-4dc1-9d3d-997d663d60bb'
+API_KEY=''
 
 # the File API URL to use
 API_SERVICE_URL=api.smartling.com # production
